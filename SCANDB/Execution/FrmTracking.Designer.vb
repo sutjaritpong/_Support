d2fd72ee594f5bc0361389d7a5557c3170f94b91
Sutjaritpong <sutjaritpong07@gmail.com>
2020-05-23 13:16:46 +0700

    update

--- SCANDB/Execution/FrmTracking.Designer.vb
+++ SCANDB/Execution/FrmTracking.Designer.vb
@@ -66,6 +66,7 @@ Partial Class FrmTracking
         '
         'cmd_find
         '
+        Me.cmd_find.BackColor = System.Drawing.SystemColors.Window
         Me.cmd_find.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
         Me.cmd_find.Image = Global.SCANDB.My.Resources.Resources.search
         Me.cmd_find.Location = New System.Drawing.Point(24, 299)
@@ -76,10 +77,11 @@ Partial Class FrmTracking
         Me.cmd_find.Text = "เลขที่บัตรประชาชน"
         Me.cmd_find.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.cmd_find.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.cmd_find.UseVisualStyleBackColor = True
+        Me.cmd_find.UseVisualStyleBackColor = False
         '
         'cmd_edit
         '
+        Me.cmd_edit.BackColor = System.Drawing.SystemColors.Window
         Me.cmd_edit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
         Me.cmd_edit.Image = Global.SCANDB.My.Resources.Resources.edit_icon16x16
         Me.cmd_edit.Location = New System.Drawing.Point(165, 299)
@@ -90,10 +92,11 @@ Partial Class FrmTracking
         Me.cmd_edit.Text = "แก้ไข"
         Me.cmd_edit.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.cmd_edit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.cmd_edit.UseVisualStyleBackColor = True
+        Me.cmd_edit.UseVisualStyleBackColor = False
         '
         'cmd_cancel
         '
+        Me.cmd_cancel.BackColor = System.Drawing.SystemColors.Window
         Me.cmd_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.cmd_cancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
         Me.cmd_cancel.Image = Global.SCANDB.My.Resources.Resources.edit_delete_icon16x16
@@ -105,10 +108,11 @@ Partial Class FrmTracking
         Me.cmd_cancel.Text = "ยกเลิก"
         Me.cmd_cancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.cmd_cancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.cmd_cancel.UseVisualStyleBackColor = True
+        Me.cmd_cancel.UseVisualStyleBackColor = False
         '
         'cmd_save
         '
+        Me.cmd_save.BackColor = System.Drawing.SystemColors.Window
         Me.cmd_save.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
         Me.cmd_save.Image = Global.SCANDB.My.Resources.Resources.Save_16x16
         Me.cmd_save.Location = New System.Drawing.Point(447, 299)
@@ -119,12 +123,12 @@ Partial Class FrmTracking
         Me.cmd_save.Text = "บันทึก"
         Me.cmd_save.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.cmd_save.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.cmd_save.UseVisualStyleBackColor = True
+        Me.cmd_save.UseVisualStyleBackColor = False
         '
         'txt_red
         '
         Me.txt_red.ForeColor = System.Drawing.Color.Red
-        Me.txt_red.Location = New System.Drawing.Point(140, 150)
+        Me.txt_red.Location = New System.Drawing.Point(140, 142)
         Me.txt_red.Name = "txt_red"
         Me.txt_red.Size = New System.Drawing.Size(158, 20)
         Me.txt_red.TabIndex = 5
@@ -135,7 +139,7 @@ Partial Class FrmTracking
         Me.Label16.BackColor = System.Drawing.SystemColors.Window
         Me.Label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label16.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label16.Location = New System.Drawing.Point(103, 176)
+        Me.Label16.Location = New System.Drawing.Point(103, 168)
         Me.Label16.Name = "Label16"
         Me.Label16.Size = New System.Drawing.Size(30, 20)
         Me.Label16.TabIndex = 116
@@ -147,7 +151,7 @@ Partial Class FrmTracking
         Me.Label12.BackColor = System.Drawing.SystemColors.Window
         Me.Label12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label12.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label12.Location = New System.Drawing.Point(73, 150)
+        Me.Label12.Location = New System.Drawing.Point(73, 142)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(60, 20)
         Me.Label12.TabIndex = 112
@@ -155,21 +159,21 @@ Partial Class FrmTracking
         '
         'txt_court
         '
-        Me.txt_court.Location = New System.Drawing.Point(140, 176)
+        Me.txt_court.Location = New System.Drawing.Point(140, 168)
         Me.txt_court.Name = "txt_court"
         Me.txt_court.Size = New System.Drawing.Size(158, 20)
         Me.txt_court.TabIndex = 6
         '
         'txt_cusid
         '
-        Me.txt_cusid.Location = New System.Drawing.Point(140, 98)
+        Me.txt_cusid.Location = New System.Drawing.Point(140, 90)
         Me.txt_cusid.Name = "txt_cusid"
         Me.txt_cusid.Size = New System.Drawing.Size(158, 20)
         Me.txt_cusid.TabIndex = 1
         '
         'txt_cusname
         '
-        Me.txt_cusname.Location = New System.Drawing.Point(140, 124)
+        Me.txt_cusname.Location = New System.Drawing.Point(140, 116)
         Me.txt_cusname.Name = "txt_cusname"
         Me.txt_cusname.Size = New System.Drawing.Size(158, 20)
         Me.txt_cusname.TabIndex = 4
@@ -180,7 +184,7 @@ Partial Class FrmTracking
         Me.Label14.BackColor = System.Drawing.SystemColors.Window
         Me.Label14.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label14.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label14.Location = New System.Drawing.Point(39, 100)
+        Me.Label14.Location = New System.Drawing.Point(39, 92)
         Me.Label14.Name = "Label14"
         Me.Label14.Size = New System.Drawing.Size(95, 20)
         Me.Label14.TabIndex = 110
@@ -192,7 +196,7 @@ Partial Class FrmTracking
         Me.Label15.BackColor = System.Drawing.SystemColors.Window
         Me.Label15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label15.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label15.Location = New System.Drawing.Point(62, 124)
+        Me.Label15.Location = New System.Drawing.Point(62, 116)
         Me.Label15.Name = "Label15"
         Me.Label15.Size = New System.Drawing.Size(72, 20)
         Me.Label15.TabIndex = 111
@@ -201,7 +205,7 @@ Partial Class FrmTracking
         'cbo_empexe
         '
         Me.cbo_empexe.FormattingEnabled = True
-        Me.cbo_empexe.Location = New System.Drawing.Point(511, 70)
+        Me.cbo_empexe.Location = New System.Drawing.Point(511, 62)
         Me.cbo_empexe.Name = "cbo_empexe"
         Me.cbo_empexe.Size = New System.Drawing.Size(232, 21)
         Me.cbo_empexe.TabIndex = 7
@@ -212,7 +216,7 @@ Partial Class FrmTracking
         Me.Label3.BackColor = System.Drawing.SystemColors.Window
         Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label3.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(409, 72)
+        Me.Label3.Location = New System.Drawing.Point(409, 64)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(96, 20)
         Me.Label3.TabIndex = 120
@@ -223,7 +227,7 @@ Partial Class FrmTracking
         Me.chk_datesend.AutoSize = True
         Me.chk_datesend.CheckAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.chk_datesend.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.chk_datesend.Location = New System.Drawing.Point(355, 122)
+        Me.chk_datesend.Location = New System.Drawing.Point(355, 114)
         Me.chk_datesend.Name = "chk_datesend"
         Me.chk_datesend.Size = New System.Drawing.Size(150, 22)
         Me.chk_datesend.TabIndex = 124
@@ -235,7 +239,7 @@ Partial Class FrmTracking
         Me.chk_datetracking.AutoSize = True
         Me.chk_datetracking.CheckAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.chk_datetracking.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.chk_datetracking.Location = New System.Drawing.Point(383, 96)
+        Me.chk_datetracking.Location = New System.Drawing.Point(383, 88)
         Me.chk_datetracking.Name = "chk_datetracking"
         Me.chk_datetracking.Size = New System.Drawing.Size(122, 22)
         Me.chk_datetracking.TabIndex = 123
@@ -245,7 +249,7 @@ Partial Class FrmTracking
         'dtp_date_send
         '
         Me.dtp_date_send.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.dtp_date_send.Location = New System.Drawing.Point(511, 123)
+        Me.dtp_date_send.Location = New System.Drawing.Point(511, 115)
         Me.dtp_date_send.Name = "dtp_date_send"
         Me.dtp_date_send.Size = New System.Drawing.Size(116, 20)
         Me.dtp_date_send.TabIndex = 9
@@ -253,7 +257,7 @@ Partial Class FrmTracking
         'dtp_date_verify
         '
         Me.dtp_date_verify.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.dtp_date_verify.Location = New System.Drawing.Point(511, 97)
+        Me.dtp_date_verify.Location = New System.Drawing.Point(511, 89)
         Me.dtp_date_verify.Name = "dtp_date_verify"
         Me.dtp_date_verify.Size = New System.Drawing.Size(116, 20)
         Me.dtp_date_verify.TabIndex = 8
@@ -261,7 +265,7 @@ Partial Class FrmTracking
         'cbo_document
         '
         Me.cbo_document.FormattingEnabled = True
-        Me.cbo_document.Location = New System.Drawing.Point(511, 149)
+        Me.cbo_document.Location = New System.Drawing.Point(511, 141)
         Me.cbo_document.Name = "cbo_document"
         Me.cbo_document.Size = New System.Drawing.Size(232, 21)
         Me.cbo_document.TabIndex = 11
@@ -272,7 +276,7 @@ Partial Class FrmTracking
         Me.Label2.BackColor = System.Drawing.SystemColors.Window
         Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label2.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(394, 150)
+        Me.Label2.Location = New System.Drawing.Point(394, 142)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(111, 20)
         Me.Label2.TabIndex = 128
@@ -284,7 +288,7 @@ Partial Class FrmTracking
         Me.Label4.BackColor = System.Drawing.SystemColors.Window
         Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label4.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(441, 176)
+        Me.Label4.Location = New System.Drawing.Point(441, 168)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(64, 20)
         Me.Label4.TabIndex = 130
@@ -292,16 +296,17 @@ Partial Class FrmTracking
         '
         'dtgv_tracking
         '
+        Me.dtgv_tracking.BackgroundColor = System.Drawing.SystemColors.HighlightText
         Me.dtgv_tracking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dtgv_tracking.Location = New System.Drawing.Point(22, 334)
         Me.dtgv_tracking.Name = "dtgv_tracking"
-        Me.dtgv_tracking.Size = New System.Drawing.Size(710, 200)
+        Me.dtgv_tracking.Size = New System.Drawing.Size(558, 200)
         Me.dtgv_tracking.TabIndex = 131
         '
         'cbo_owner
         '
         Me.cbo_owner.FormattingEnabled = True
-        Me.cbo_owner.Location = New System.Drawing.Point(140, 73)
+        Me.cbo_owner.Location = New System.Drawing.Point(140, 65)
         Me.cbo_owner.Name = "cbo_owner"
         Me.cbo_owner.Size = New System.Drawing.Size(158, 21)
         Me.cbo_owner.TabIndex = 3
@@ -312,7 +317,7 @@ Partial Class FrmTracking
         Me.Label10.BackColor = System.Drawing.SystemColors.Window
         Me.Label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Label10.Font = New System.Drawing.Font("Bookman Old Style", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label10.Location = New System.Drawing.Point(57, 72)
+        Me.Label10.Location = New System.Drawing.Point(57, 64)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(76, 20)
         Me.Label10.TabIndex = 132
@@ -321,7 +326,7 @@ Partial Class FrmTracking
         'cbo_detail
         '
         Me.cbo_detail.FormattingEnabled = True
-        Me.cbo_detail.Location = New System.Drawing.Point(511, 176)
+        Me.cbo_detail.Location = New System.Drawing.Point(511, 168)
         Me.cbo_detail.Name = "cbo_detail"
         Me.cbo_detail.Size = New System.Drawing.Size(232, 21)
         Me.cbo_detail.TabIndex = 12
@@ -330,7 +335,7 @@ Partial Class FrmTracking
         '
         Me.cbo_waning.ForeColor = System.Drawing.Color.Red
         Me.cbo_waning.FormattingEnabled = True
-        Me.cbo_waning.Location = New System.Drawing.Point(633, 122)
+        Me.cbo_waning.Location = New System.Drawing.Point(633, 114)
         Me.cbo_waning.Name = "cbo_waning"
         Me.cbo_waning.Size = New System.Drawing.Size(160, 21)
         Me.cbo_waning.TabIndex = 133
